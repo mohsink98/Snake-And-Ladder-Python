d5faa3eda5459fab4ddda9e47cf5f06f3b28cb5b
Mohsin <mohsink9899@gmail.com>
2021-09-10 15:31:56 +0530

    Added Functionality to how to play function and created continue game function for modularity

--- funcs.py
+++ funcs.py
@@ -1,6 +1,16 @@
 import os
 clear = lambda: os.system('cls')
 
+def continue_game():
+    user_choice = input('''
+    Press 0 to go back to main menu
+    Type 'Exit' to exit the game.
+        ''')
+    if user_choice == '0':
+        main_menu()
+    elif user_choice.lower() == 'exit':
+        exit()
+
 def start_game():
     player1 = input("Enter name of Player 1 : ")
     player2 = input("Enter name of Player 2 : ")
@@ -8,33 +18,40 @@ def start_game():
 def rules():
     clear()
     print('''
-        1. When a piece comes on a number which lies on the top of a snake (face of the snake), 
-            then the piece/token will land below to the bottom of the snake (tail of it) that can also be said as an unlucky move.
+    1. When a piece comes on a number which lies on the top of a snake (face of the snake), 
+        then the piece/token will land below to the bottom of the snake (tail of it) that can also be said as an unlucky move.
 
-        2. If somehow the piece falls on the ladder base, 
-            it will immediately climb to the top of the ladder (which is considered to be a lucky move).
-        
-        3. Whereas if a player lands on the bottom of the snake or top of a ladder, 
-            the player will remain in the same spot (same number) and will not get affected by any particular rule. The players can never move down ladders.
-        
-        4. The pieces of different players can overlap each other without knocking out anyone. 
-            There is no concept of knocking out by opponent players in Snakes and Ladders.
-        
-        5. To win, the player needs to roll the exact number of die to land on the number 100. 
-            If he/she fails to do so, then the player needs to roll the die again in the next turn. 
-            For example, if a player is on the number 98 and the die roll shows the number 4, then the player cannot move its piece until he/she gets a 2 to win or 1 to be on the 99th number.
+    2. If somehow the piece falls on the ladder base, 
+        it will immediately climb to the top of the ladder (which is considered to be a lucky move).
+    
+    3. Whereas if a player lands on the bottom of the snake or top of a ladder, 
+        the player will remain in the same spot (same number) and will not get affected by any particular rule. The players can never move down ladders.
+    
+    4. The pieces of different players can overlap each other without knocking out anyone. 
+        There is no concept of knocking out by opponent players in Snakes and Ladders.
+    
+    5. To win, the player needs to roll the exact number of die to land on the number 100. 
+        If he/she fails to do so, then the player needs to roll the die again in the next turn. 
+        For example, if a player is on the number 98 and the die roll shows the number 4, 
+        then the player cannot move its piece until he/she gets a 2 to win or 1 to be on the 99th number.
     ''')
-    user_choice = input('''
-            Press 0 to go back to main main menu
-            Type 'Exit' to exit the game.
-        ''')
-    if user_choice == '0':
-        main_menu()
-    elif user_choice.lower() == 'exit':
-        exit()
+    continue_game()
+    
 
 def how_to_play():
-    pass
+    clear()
+    print('''
+    1. Each player puts their name and tha game starts.
+    
+    2. Take it in turns to roll the dice. Counter automatically moves forward the number of spaces shown on the dice.
+    
+    3. If your counter lands at the bottom of a ladder, you can move up to the top of the ladder.
+    
+    4. If your counter lands on the head of a snake, you must slide down to the bottom of the snake.
+    
+    5. The first player to get to the space that says 'home' is the winner.
+    ''')
+    continue_game()
 
 def settings():
     pass
@@ -69,6 +86,5 @@ def main_menu():
     main_menu_options(choice)
 
 
-main_menu()
 if __name__== '__main__':
-    print("Hello, User!")
+    main_menu()
--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 import funcs
+
+funcs.main_menu()
